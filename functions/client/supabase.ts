b762082f68038a522c3f884f877bcb8102cef97c
Manuel <58395553+cr4yfish@users.noreply.github.com>
2024-10-12 20:25:23 +0200

    Added image fetching to user profile

--- functions/client/supabase.ts
+++ functions/client/supabase.ts
@@ -1,5 +1,6 @@
 
 import { createClient, User } from "@supabase/supabase-js";
+import { FileObject } from "@supabase/storage-js";
 
 import { Course, Profile, Question, Rank, Settings, Topic, User_Course, User_Question, User_Topic } from "@/types/db";
 import { SessionState } from "@/types/auth";
@@ -88,6 +89,16 @@ export async function getRanks(): Promise<Rank[]> {
 export async function getProfile(id: string): Promise<Profile> {
     const { data, error } = await getClient().from("profiles").select().eq("id", id);
     if(error) { throw error; }
+
+    const profile = data[0] as Profile;
+
+    // get links
+    try {
+        if(profile.avatar) profile.avatarLink = await getObjectPublicURL({ id: profile.avatar, bucket: "avatars" });
+        if(profile.banner) profile.bannerLink = await getObjectPublicURL({ id: profile.banner, bucket: "banners" });   
+    } catch (error) {
+        console.error("Error getting profile links:", error);
+    }
     return data[0];
 }
 
@@ -282,4 +293,103 @@ export async function addUsersTopics({ userID, topicID, completed } : { userID: 
     }]).select();
     if(error) { throw error; }
     return data[0];
+}
+
+/**
+ * Download object either by providing filename and path or the full path
+ * @param params 
+ */
+export async function downloadObject(params: { filename: string, path: string }): Promise<any>;
+export async function downloadObject(params: { fullPath: string }): Promise<any>;
+
+/**
+ * Download object either by providing filename and path or the full path
+ * @param params 
+ * @returns 
+ */
+export async function downloadObject(params: { filename?: string, path?: string, fullPath?: string }): Promise<Blob> {
+    let fullPath = params.fullPath;
+    if (!fullPath && params.filename && params.path) {
+        fullPath = params.path + params.filename;
+    }
+    if (!fullPath) {
+        throw new Error("Invalid parameters");
+    }
+    const { data, error } = await getClient().storage.from("objects").download(fullPath);
+    if(error) { throw error; }
+    return data;
+}
+
+/**
+ * Gets a signed URL for an object valid for 60 minutes
+ * @param params 
+ */
+export async function getObjectSignedURL(params: { filename: string, path: string }): Promise<string>;
+export async function getObjectSignedURL(params: { fullPath: string }): Promise<string>;
+
+export async function getObjectSignedURL(params: { filename?: string, path?: string, fullPath?: string }): Promise<string> {
+    let fullPath = params.fullPath;
+    if (!fullPath && params.filename && params.path) {
+        fullPath = params.path + params.filename;
+    }
+    if (!fullPath) {
+        throw new Error("Invalid parameters");
+    }
+    const res = await getClient().storage.from("objects").createSignedUrl(fullPath, 60);
+
+    if(res.error) {
+        throw res.error;
+    }
+
+    return res.data.signedUrl;
+}
+
+/**
+ * Wrapper for : A simple convenience function to get the URL for an asset in a public bucket. If you do not want to use this function, you can construct the public URL by concatenating the bucket URL with the path to the asset. This function does not verify if the bucket is public. If a public URL is created for a bucket which is not public, you will not be able to download the asset.
+ * @param params 
+ */
+export async function getObjectPublicURL(params: { filename: string, path: string, bucket: string }): Promise<string>;
+export async function getObjectPublicURL(params: { fullPath: string, bucket: string }): Promise<string>;
+export async function getObjectPublicURL(params: { id: string, bucket: string }): Promise<string>;
+
+export async function getObjectPublicURL(params: { filename?: string, path?: string, fullPath?: string, id?: string, bucket: string }): Promise<string> {
+    if(!params.bucket) {
+        throw new Error("Bucket is required");
+    }
+    
+    let fullPath = params.fullPath;
+    if (!fullPath && params.filename && params.path) {
+        fullPath = params.path + params.filename;
+    }
+    if (fullPath) {
+        return (getClient().storage.from(params.bucket).getPublicUrl(fullPath).data.publicUrl);
+    }
+    if(params.id) {
+        try {
+            const fileObject = await getObjectInfoFromID(params.id, params.bucket);
+            console.log("File object:", fileObject, "for id:", params.id);
+            return getClient().storage.from(params.bucket).getPublicUrl(fileObject.name).data.publicUrl;
+        } catch (error) {
+            throw error;
+        }
+    }
+    
+    throw new Error("Invalid parameters");
+}
+
+export async function getObjectInfoFromID(id: string, bucket: string): Promise<FileObject> {
+    const res = await getClient().storage.from(bucket).list();
+
+    console.log("Objects:", res.data);
+
+    if(res.error) {
+        throw res.error;
+    }
+    const fileObject = res.data.find((obj: FileObject) => obj.id === id);
+
+    if(!fileObject) {
+        throw new Error("File object not found");
+    }
+
+    return fileObject;
 }
