136f9aa485b3cdff4b18642178e60ed55d9532dc
Manuel <58395553+cr4yfish@users.noreply.github.com>
2024-10-14 22:09:02 +0200

    Added icon smaller prop

--- components/Icon.tsx
+++ components/Icon.tsx
@@ -2,10 +2,17 @@ import React from "react";
 
 import "@material-symbols/font-400";
 
-export default function Icon({ children, color, filled, upscale } : { children: React.ReactNode, color?: string, filled?: boolean, upscale?: boolean }) {
+export default function Icon(
+    { children, color, filled, upscale, downscale } : 
+    { children: React.ReactNode, color?: string, filled?: boolean, upscale?: boolean, downscale?: boolean }) {
     return (
         <span 
-            className={"material-symbols-rounded prose dark:prose-invert text-5xl" + (color ? ` text-${color}` : "") + (filled ? " material-symbols-filled " : "") + (upscale ? "scale-125" : "")}
+            className={
+                "material-symbols-rounded prose dark:prose-invert text-tiny" 
+                + (color ? ` text-${color}` : "") 
+                + (filled ? " material-symbols-filled " : "") 
+                + (upscale ? "scale-125" : "") + (downscale ? "scale-75" : "")
+            }
         >
             {children}
         </span>
