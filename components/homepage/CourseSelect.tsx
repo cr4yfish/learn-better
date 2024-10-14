27d0fad4ad0b3437a66b6ec983b51b33f3268d68
Manuel <58395553+cr4yfish@users.noreply.github.com>
2024-10-14 21:38:24 +0200

    improved course select UX

--- components/homepage/CourseSelect.tsx
+++ components/homepage/CourseSelect.tsx
@@ -1,5 +1,7 @@
+import { useState, useEffect } from 'react';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import "swiper/css";
+import { Swiper as SwiperType } from 'swiper/types';
 
 import CourseButton from './CourseButton';
 import { User_Course } from '@/types/db';
@@ -10,7 +12,14 @@ export default function CourseSelect({
         userCourses: User_Course[], currentUserCourse: User_Course, 
         setCurrentUserCourse: React.Dispatch<React.SetStateAction<User_Course>> 
     }) {
+    const [swiper, setSwiper] = useState<SwiperType | undefined>();
 
+    // slide to current course when it changes
+    // current slide moves to first slide
+    useEffect(() => {
+        if(swiper) { swiper?.slideTo(0) }
+    }, [swiper, currentUserCourse])
+    
     return (
         <div
             className='w-full max-h-96 max-w-full py-4 backdrop-blur relative overflow-auto'
@@ -20,9 +29,10 @@ export default function CourseSelect({
                 slidesPerView={3}
                 loop={false}
                 simulateTouch
+                onSwiper={setSwiper}
                 className=' w-full h-full max-w-full overflow-x-scroll'
             >
-                {userCourses.map((userCourse) => (
+                {[currentUserCourse, ...userCourses.filter((uc) => uc.course.id !== currentUserCourse.course.id )].map((userCourse) => (
                     <SwiperSlide className=' w-fit min-h-full' key={userCourse.course.id}>
                         <CourseButton 
                             onPress={() => setCurrentUserCourse(userCourse)} 
