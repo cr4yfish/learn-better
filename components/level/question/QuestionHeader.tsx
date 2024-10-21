98fbf7982bd00657bb60778d8aa8f36cf89d19ff
Manuel <58395553+cr4yfish@users.noreply.github.com>
2024-10-21 19:06:00 +0200

    Moved loaded state to outside component

--- components/level/question/QuestionHeader.tsx
+++ components/level/question/QuestionHeader.tsx
@@ -6,19 +6,19 @@ import { Button } from "@/components/utils/Button";
 import Icon from "../../utils/Icon";
 import Xp from "../../utils/Xp";
 
-export default function QuestionHeader({ progress, xp, numQuestions } : { progress: number, xp: number, numQuestions: number }) {
+export default function QuestionHeader({ progress, xp, numQuestions, show } : { progress: number, xp: number, numQuestions: number, show: boolean }) {
 
     return (
         <>
-            { progress !== numQuestions &&
-                <div className="flex flex-row gap-2 items-center justify-center w-full">
-                <Link href="/"><Button isIconOnly variant="light"><Icon>close</Icon></Button></Link>
-                <div className="flex flex-col gap-1 items-center justify-center w-full">
-                    <Progress value={progress} maxValue={numQuestions} color={progress == numQuestions ? "success" : "primary"} />
-                </div>
-                <Xp xp={xp} />
+        { show &&
+            <div className="flex flex-row gap-2 items-center justify-center w-full">
+            <Link href="/"><Button isIconOnly variant="light"><Icon>close</Icon></Button></Link>
+            <div className="flex flex-col gap-1 items-center justify-center w-full">
+                <Progress value={progress} maxValue={numQuestions} color={progress == numQuestions ? "success" : "primary"} />
             </div>
-            }
+            <Xp xp={xp} isLoaded={true} />
+        </div>
+        }
         </>
     )
 
