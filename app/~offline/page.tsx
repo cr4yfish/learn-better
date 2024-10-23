6ec149a4e4623a518a1709c91c54a1a3b8a0588d
Manuel <58395553+cr4yfish@users.noreply.github.com>
2024-10-23 11:30:58 +0200

    added offline fallback & pwa options

---
+++ app/~offline/page.tsx
@@ -0,0 +1,9 @@
+
+
+export default function Offline() {
+    return (
+      <>
+        <h1>This is offline fallback page test</h1>
+      </>
+    );
+  }
--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,11 @@ import withPWAInit from "@ducanh2912/next-pwa";
 
 const withPWA = withPWAInit({
   dest: "public",
+  cacheOnFrontEndNav: true,
+  aggressiveFrontEndNavCaching: true,
+  cacheStartUrl: true,
+  dynamicStartUrl: true,
+  dynamicStartUrlRedirect: "/auth"
 });
 
 export default withPWA({
