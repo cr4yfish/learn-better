fe8c0866cf86d82d99eaa35d29af4727dd154dbd
Manuel <58395553+cr4yfish@users.noreply.github.com>
2024-10-23 08:37:47 +0200

    Improved new course UI

--- app/course/new/[userId]/page.tsx
+++ app/course/new/[userId]/page.tsx
@@ -1,6 +1,9 @@
 "use server";
 
 import EditCourseCard from "@/components/course/EditCourseCard";
+import { Button } from "@/components/utils/Button";
+import Icon from "@/components/utils/Icon";
+import Link from "next/link";
 
 export default async function NewCourse({ params: { userId }} : { params: { userId: string }}) {
 
@@ -16,8 +19,13 @@ export default async function NewCourse({ params: { userId }} : { params: { user
     }
 
     return (
-    <>
-        <EditCourseCard userId={userId} isNew={true}  />
-    </>
+        <div className="px-4 py-6 flex flex-col gap-2">
+            <Link href="/">
+                <Button variant="light" startContent={<Icon filled>arrow_back</Icon>} >
+                    Back
+                </Button>
+            </Link>
+            <EditCourseCard userId={userId} isNew={true}  />
+        </div>
   );
 }
