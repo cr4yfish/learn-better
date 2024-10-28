4220abf465e11a1385b5aa24af76b2e69bf7d620
Manuel <58395553+cr4yfish@users.noreply.github.com>
2024-10-28 18:54:32 +0100

    Added first step for training page

--- app/training/page.tsx
+++ app/training/page.tsx
@@ -1,14 +1,20 @@
 "use server";
 
+import { Button } from "@/components/utils/Button";
 import Navigation from "@/components/utils/Navigation"
+import { getWeakQuestions } from "@/utils/supabase/questions";
 
 
 export default async function Training() {
+
+    const weakQuestions = await getWeakQuestions();
     
     return (
         <>
-        <div className=" px-4 py-6 flex">
+        <div className=" px-4 py-6 flex flex-col gap-4">
             <h1 className=" font-bold">Training</h1>
+            <span>Weak Questions : {weakQuestions.length}</span>
+            <Button isDisabled color="primary" variant="shadow">Train</Button>
         </div>
         
         <Navigation activeTitle="Training" />
