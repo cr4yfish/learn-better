77cf92aadf2414230c44572dd3aacdd3b026ac5e
Manuel <58395553+cr4yfish@users.noreply.github.com>
2024-10-28 15:03:09 +0100

    Added seperate dark color for icons

--- components/utils/Icon.tsx
+++ components/utils/Icon.tsx
@@ -3,13 +3,14 @@ import React from "react";
 import "@material-symbols/font-400";
 
 export default function Icon(
-    { children, color="inherit", filled, upscale, downscale } : 
-    { children: React.ReactNode, color?: string, filled?: boolean, upscale?: boolean, downscale?: boolean }) {
+    { children, color="inherit", darkColor, filled, upscale, downscale } : 
+    { children: React.ReactNode, color?: string, darkColor?: string; filled?: boolean, upscale?: boolean, downscale?: boolean }) {
     return (
         <span 
             className={
                 "material-symbols-rounded prose dark:prose-invert text-tiny" 
-                + (color ? ` text-${color}` : "") 
+                + (color ? ` text-${color}` : "")
+                + (darkColor ? ` dark:text-${darkColor}` : "") 
                 + (filled ? " material-symbols-filled " : "") 
                 + (upscale ? "scale-125" : "") + (downscale ? "scale-75" : "")
             }
