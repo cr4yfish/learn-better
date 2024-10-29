9b3c33fac83241e0fe54789a2e3f969498e3d49c
Manuel <58395553+cr4yfish@users.noreply.github.com>
2024-10-29 17:44:00 +0100

    Improved dark/light mode readability

--- components/ui/card.tsx
+++ components/ui/card.tsx
@@ -53,7 +53,7 @@ const CardDescription = React.forwardRef<
 >(({ className, ...props }, ref) => (
   <p
     ref={ref}
-    className={cn("text-sm text-neutral-500 dark:text-neutral-400", className)}
+    className={cn("text-sm text-neutral-700 dark:text-neutral-300", className)}
     {...props}
   />
 ))
