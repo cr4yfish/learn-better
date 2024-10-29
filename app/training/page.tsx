59b4f5dd4fc75097f6f6960d11b68eb5c54d5014
Manuel <58395553+cr4yfish@users.noreply.github.com>
2024-10-29 18:27:09 +0100

    added scrolling to training page

--- app/training/page.tsx
+++ app/training/page.tsx
@@ -5,6 +5,7 @@ import { Button } from "@/components/utils/Button";
 import Icon from "@/components/utils/Icon";
 import Navigation from "@/components/utils/Navigation"
 import { getWeakQuestions } from "@/utils/supabase/questions";
+import { ScrollShadow } from "@nextui-org/scroll-shadow";
 
 
 export default async function Training() {
@@ -13,64 +14,67 @@ export default async function Training() {
     
     return (
         <>
-        <div className=" px-4 py-6 flex flex-col gap-4">
-            <h1 className="text-2xl font-bold">Training</h1>
-            <Card>
-                <CardHeader>
-                    <CardDescription className="flex flex-row items-center gap-1">
-                        <Icon downscale>exercise</Icon>
-                        Improve your weaknesses
-                    </CardDescription>
-                    <CardTitle>You&apos;ve got {weakQuestions.length} weak questions</CardTitle>
-                </CardHeader>
-                <CardContent className="flex flex-row items-center gap-2">
-                    <Button startContent={<Icon filled>exercise</Icon>} fullWidth size="lg" color="primary" variant="shadow">Train</Button>
-                    <Button startContent={<Icon filled>list</Icon>} size="lg" className="w-[150px]" color="secondary" variant="flat">View</Button>
-                </CardContent>
-            </Card>
+        <ScrollShadow className="pb-20">
+            <div className=" px-4 py-6 flex flex-col gap-4 overflow-y-visible min-h-fit">
+                <h1 className="text-2xl font-bold">Training</h1>
+                <Card>
+                    <CardHeader>
+                        <CardDescription className="flex flex-row items-center gap-1">
+                            <Icon downscale>exercise</Icon>
+                            Improve your weaknesses
+                        </CardDescription>
+                        <CardTitle>You&apos;ve got {weakQuestions.length} weak questions</CardTitle>
+                    </CardHeader>
+                    <CardContent className="flex flex-row items-center gap-2">
+                        <Button startContent={<Icon filled>exercise</Icon>} fullWidth size="lg" color="primary" variant="shadow">Train</Button>
+                        <Button startContent={<Icon filled>list</Icon>} size="lg" className="w-[150px]" color="secondary" variant="flat">View</Button>
+                    </CardContent>
+                </Card>
 
-            
-            <Card>
-                <CardHeader>
-                    <CardDescription className="flex flex-row items-center gap-1">
-                        <Icon downscale>calendar_month</Icon>
-                        Weekly goal
-                    </CardDescription>
-                    <CardTitle>Your weekly goal</CardTitle>
-                </CardHeader>
-                <CardContent>
-                    <div className="flex flex-row justify-evenly items-center w-full">
-                        <Button isIconOnly variant="flat"><Icon>remove</Icon></Button>
-                        <div className="flex flex-col items-center justify-center">
-                            <span className="text-4xl font-bold">25</span>
-                            <span className="text-tiny text-gray-700 dark:text-gray-300">Minutes/Day</span>
+                
+                <Card>
+                    <CardHeader>
+                        <CardDescription className="flex flex-row items-center gap-1">
+                            <Icon downscale>calendar_month</Icon>
+                            Weekly goal
+                        </CardDescription>
+                        <CardTitle>Your weekly goal</CardTitle>
+                    </CardHeader>
+                    <CardContent>
+                        <div className="flex flex-row justify-evenly items-center w-full">
+                            <Button isIconOnly variant="flat"><Icon>remove</Icon></Button>
+                            <div className="flex flex-col items-center justify-center">
+                                <span className="text-4xl font-bold">25</span>
+                                <span className="text-tiny text-gray-700 dark:text-gray-300">Minutes/Day</span>
+                            </div>
+                            <Button isIconOnly variant="flat"><Icon>add</Icon></Button>
                         </div>
-                        <Button isIconOnly variant="flat"><Icon>add</Icon></Button>
-                    </div>
-                </CardContent>
-            </Card>
+                    </CardContent>
+                </Card>
 
-            <Card>
-                <CardHeader>
-                    <CardDescription className="flex flex-row items-center gap-1">
-                        <Icon downscale>group</Icon>
-                        Exercise with friends
-                    </CardDescription>
-                    <CardTitle>Your Quests</CardTitle>
-                </CardHeader>
-                <CardContent className="flex items-center justify-center">
-                    <span className=" text-gray-700 dark:text-gray-300 ">No ongoing Quests</span>
-                </CardContent>
-                <CardFooter>
-                    <Button startContent={<Icon filled>group</Icon>} fullWidth size="lg" color="primary" variant="shadow">Start a Quest</Button>
-                </CardFooter>
-            </Card>
+                <Card>
+                    <CardHeader>
+                        <CardDescription className="flex flex-row items-center gap-1">
+                            <Icon downscale>group</Icon>
+                            Exercise with friends
+                        </CardDescription>
+                        <CardTitle>Your Quests</CardTitle>
+                    </CardHeader>
+                    <CardContent className="flex items-center justify-center">
+                        <span className=" text-gray-700 dark:text-gray-300 ">No ongoing Quests</span>
+                    </CardContent>
+                    <CardFooter>
+                        <Button startContent={<Icon filled>group</Icon>} fullWidth size="lg" color="primary" variant="shadow">Start a Quest</Button>
+                    </CardFooter>
+                </Card>
 
-            
+                
 
-        </div>
-        
+            </div>
+        </ScrollShadow>
+
         <Navigation activeTitle="Training" />
+        
         </>
     )
 }
