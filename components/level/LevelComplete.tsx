6af61ba8f7c6d40c64fb4b1426b0c4fbede913f9
Manuel <58395553+cr4yfish@users.noreply.github.com>
2024-10-31 15:42:28 +0100

    Switched level&training complete to use the new component

--- components/level/LevelComplete.tsx
+++ components/level/LevelComplete.tsx
@@ -1,46 +1,18 @@
-import { Card, CardBody } from "@nextui-org/card"
-
-import Icon from "../utils/Icon"
 
 import { User_Topic } from "@/types/db"
-import { formatSeconds } from "@/functions/helpers"
+import LevelCompleteStats from "./complete/LevelCompleteStats"
 
 export default function LevelComplete({ userTopic } : { userTopic: User_Topic }) {
 
     return (
         <>
-        <div className="flex flex-col gap-4 w-full items-center justify-evenly">
+        <div className="flex flex-col gap-4 w-full items-center justify-evenly relative z-50">
 
-            <Card className=" w-full py-2 bg-success/15 text-success font-bold text-lg">
-                <CardBody className="flex flex-row items-center w-full justify-between">
-                    <span>Stars found</span>
-                    <div className="flex items-center gap-1">
-                        <Icon color="success" filled>hotel_class</Icon>
-                        {userTopic.xp}
-                    </div>
-                    
-                </CardBody>
-            </Card>
-
-            <Card className=" w-full py-2 bg-warning/15 text-warning font-bold text-lg">
-                <CardBody className="flex flex-row items-center w-full justify-between">
-                    <span>Time</span>
-                    <div className="flex items-center gap-1">
-                        <Icon color="warning" filled>timer</Icon>
-                        {formatSeconds(userTopic.seconds)}
-                    </div>
-                </CardBody>
-            </Card>
-
-            <Card className=" w-full py-2 bg-danger/15 text-danger font-bold text-lg">
-                <CardBody className="flex flex-row items-center w-full justify-between">
-                    <span>Accuracy</span>
-                    <div className="flex items-center gap-1">
-                        <Icon color="danger" filled>crisis_alert</Icon>
-                        {Math.round(userTopic.accuracy)}%
-                    </div>
-                </CardBody>
-            </Card>
+            <LevelCompleteStats 
+                xp={userTopic.xp}
+                seconds={userTopic.seconds}
+                accuracy={userTopic.accuracy}
+            />
 
         </div>
         </>
