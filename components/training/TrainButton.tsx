620757c5d55783cda7350cf4a877f3019c50acfd
Manuel <58395553+cr4yfish@users.noreply.github.com>
2024-10-31 12:26:07 +0100

    Added loading to training button

--- components/training/TrainButton.tsx
+++ components/training/TrainButton.tsx
@@ -1,5 +1,7 @@
 "use client";
 
+import { useState } from "react";
+
 import { Weak_User_Questions } from "@/types/db";
 import { Button } from "../utils/Button";
 import Icon from "../utils/Icon";
@@ -7,11 +9,14 @@ import Icon from "../utils/Icon";
 import { createTrainingLevel } from "@/app/training/actions";
 
 export default function TrainButton({ weakQuestions } : { weakQuestions: Weak_User_Questions[] }) {
+    const [isLoading, setIsLoading] = useState(false);
+
     return (
         <Button 
             startContent={<Icon filled>exercise</Icon>} 
             fullWidth size="lg" color="primary" variant="shadow"
-            onClick={() => createTrainingLevel(weakQuestions)}
+            onClick={() => {createTrainingLevel(weakQuestions); setIsLoading(true)}}
+            isLoading={isLoading}
         >
             Train
         </Button>
