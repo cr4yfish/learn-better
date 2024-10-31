58b9c1e4bd05471dbc44ccf3442f15cd6bb4e879
Manuel <58395553+cr4yfish@users.noreply.github.com>
2024-10-31 11:20:41 +0100

    Fixed level scroller double scroll

--- components/levelScroller/LevelScroller.tsx
+++ components/levelScroller/LevelScroller.tsx
@@ -179,7 +179,7 @@ export default function LevelScroller({ initUserCourse, initTopics } : { initUse
     <>
         <InfiniteScroll 
             id="infiniteScroll"
-            className="flex flex-col items-center gap-4 overflow-x-hidden"
+            className="flex flex-col items-center gap-4 overflow-x-hidden overflow-y-hidden"
             pageStart={1}
             loadMore={async () => await handleLoadMore()}
             hasMore={canLoadMore}
