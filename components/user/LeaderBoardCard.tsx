53f118948272f7d9cd6373c98afac8ba3594f46c
Manuel <58395553+cr4yfish@users.noreply.github.com>
2024-11-03 23:04:00 +0100

    enabled leaderboard position numbers on all cards

--- components/user/LeaderBoardCard.tsx
+++ components/user/LeaderBoardCard.tsx
@@ -14,14 +14,15 @@ export default function LeaderboardCard({ profile, sessionState, position } : { 
         <Card key={profile.id} className=" w-full" isPressable>
             <CardBody className=" w-full flex flex-row justify-between items-center dark:text-gray-200 text-gray-800">
                 <div className="h-full flex flex-row items-center">
-                    { position < 4 &&
+                    { 
                         <div className="h-full w-[35px] pl-2 flex items-center justify-start">
                             <span 
                                 className={
                                     `text-lg font-black 
-                                    ${position == 1 && "text-orange-300"}
-                                    ${position == 2 && "text-gray-400"}
-                                    ${position == 3 && "text-yellow-600"}
+                                    text-gray-700 dark:text-gray-500
+                                    ${position == 1 && "text-orange-400 dark:text-orange-300"}
+                                    ${position == 2 && "text-gray-500 dark:text-gray-300"}
+                                    ${position == 3 && "text-yellow-500 dark:text-yellow-600"}
                                 `}>
                                     {position}
                                 </span>
